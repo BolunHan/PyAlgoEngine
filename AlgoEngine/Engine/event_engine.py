086b5ad13f83b025b48f862c09f4fba8a3d5b706
Bolun <Bolun.Han@outlook.com>
2024-05-28 19:10:47 +0800

    updated to use new event_engine

--- AlgoEngine/Engine/event_engine.py
+++ AlgoEngine/Engine/event_engine.py
@@ -1,32 +1,33 @@
 from types import SimpleNamespace
 
-import EventEngine
+import event_engine
+from event_engine import Topic, PatternTopic, EventEngine
 
 from . import LOGGER
 
 __all__ = ['EVENT_ENGINE', 'TOPIC']
 LOGGER = LOGGER.getChild('EventEngine')
-EventEngine.set_logger(LOGGER)
+event_engine.set_logger(LOGGER)
 
 
 class TopicSet(object):
-    on_order = EventEngine.Topic('on_order')
-    on_report = EventEngine.Topic('on_report')
-    eod = EventEngine.Topic('eod')
-    eod_done = EventEngine.Topic('eod_done')
-    bod = EventEngine.Topic('bod')
-    bod_done = EventEngine.Topic('bod_done')
+    on_order = Topic('on_order')
+    on_report = Topic('on_report')
+    eod = Topic('eod')
+    eod_done = Topic('eod_done')
+    bod = Topic('bod')
+    bod_done = Topic('bod_done')
 
-    launch_order = EventEngine.PatternTopic('launch_order.{ticker}')
-    cancel_order = EventEngine.PatternTopic('cancel_order.{ticker}')
-    realtime = EventEngine.PatternTopic('realtime.{ticker}.{dtype}')
+    launch_order = PatternTopic('launch_order.{ticker}')
+    cancel_order = PatternTopic('cancel_order.{ticker}')
+    realtime = PatternTopic('realtime.{ticker}.{dtype}')
 
     @classmethod
     def push(cls, market_data):
         return cls.realtime(ticker=market_data.ticker, dtype=market_data.__class__.__name__)
 
     @classmethod
-    def parse(cls, topic: EventEngine.Topic) -> SimpleNamespace:
+    def parse(cls, topic: Topic) -> SimpleNamespace:
         try:
             _ = topic.value.split('.')
 
@@ -47,6 +48,6 @@ class TopicSet(object):
             raise ValueError(f'Invalid topic {topic}')
 
 
-EVENT_ENGINE = EventEngine.EventEngine()
+EVENT_ENGINE = EventEngine()
 TOPIC = TopicSet
 # EVENT_ENGINE.start()
