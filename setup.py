c38752b06be30929c7430e43e8c2ad95c00de2f6
Bolun <Bolun.Han@outlook.com>
2024-05-29 15:17:23 +0800

    updated requirements

--- setup.py
+++ setup.py
@@ -39,14 +39,16 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.1',
     install_requires=[
         'numpy',
         'pandas',
         'exchange_calendars',
-        'PyQuantKit',
-        'PyEventEngine',
+        'PyQuantKit>=0.3.0',
+        'PyEventEngine>=0.3.0.post3',
     ]
 )
