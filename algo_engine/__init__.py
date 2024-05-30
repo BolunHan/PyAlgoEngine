5d4bde00b9bf6097c39c0cbb1c758519846af262
Bolun <Bolun.Han@outlook.com>
2024-05-30 12:13:06 +0800

    v0.4.0.post2

--- algo_engine/__init__.py
+++ algo_engine/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.0.post1"
+__version__ = "0.4.0.post2"
 
 import logging
 import os
--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
         'numpy',
         'pandas',
         'exchange_calendars',
-        'PyQuantKit>=0.3.0',
-        'PyEventEngine>=0.3.0.post3',
+        'PyQuantKit>=0.3.1',
+        'PyEventEngine>=0.3.0.post4',
     ]
 )
