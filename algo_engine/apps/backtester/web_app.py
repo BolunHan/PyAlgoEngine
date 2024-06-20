cf89f111882a6bc48633428bf945d16127678c7e
Bolun <Bolun.Han@outlook.com>
2024-06-20 15:57:03 +0800

    resolved flask double logging issue

--- algo_engine/apps/backtester/web_app.py
+++ algo_engine/apps/backtester/web_app.py
@@ -1,84 +1,79 @@
-import pathlib
 from threading import Thread
 
+from bokeh.embed import server_document
 from flask import Flask, render_template
-from tornado.ioloop import IOLoop
+from waitress import serve
 
-from .doc_server import DocServer
-from . import LOGGER
+from .doc_server import CandleStick
+from ..bokeh_server import DocManager
+from .. import LOGGER
 
 
-class DocManager(object):
-    def __init__(self, port: int = 21543, io_loop: IOLoop = None, **kwargs):
+class WebApp(object):
+    def __init__(self, name: str = 'WebApp.Backtest', address: str = '0.0.0.0', port: int = 8080, **kwargs):
+        self.name = name
+
+        self.host = address
         self.port = port
-        self.io_loop = IOLoop() if io_loop is None else io_loop
 
-        self.doc_server: dict[str, DocServer] = {}
-        self.bokeh_address = kwargs.get('bokeh_address', 'localhost')
-        self.bokeh_port = kwargs.get('bokeh_port', 5006)
-        self.bokeh_thread = Thread(target=self.serve_bokeh, daemon=True)
+        self.flask = Flask(import_name=name)
+        self.doc_manager = DocManager(host='localhost', port=port)
+        self.candle_sticks = {}
 
-    def register(self, ticker: str, doc_server: DocServer = None, url: str = None, **kwargs):
-        if url is None:
-            url = f'/{ticker}'
+    def update(self, **kwargs):
+        for doc_server in self.doc_manager.doc_server.values():
+            doc_server.update(**kwargs)
 
-        if doc_server is None:
-            doc_server = DocServer(ticker=ticker, url=url, **kwargs)
+    def register(self, ticker: str, **kwargs):
+        self.candle_sticks[ticker] = candlestick = CandleStick(ticker=ticker, url=f'/candlesticks/{ticker}', **kwargs)
 
-        self.doc_server[ticker] = doc_server
-        return doc_server
+        self.doc_manager.register(doc_server=candlestick)
 
-    def serve_bokeh(self):
-        from bokeh.server.server import Server
+    def index(self):
+        embedded_docs = []
 
-        applications = {doc_server.url: doc_server.__call__ for doc_server in self.doc_server.values()}
-        server = Server(
-            applications=applications,
-            # io_loop=self.io_loop,
-            address='localhost',
-            port=self.bokeh_port,
-            allow_websocket_origin=[f"localhost:{self.bokeh_port}", f"127.0.0.1:{self.bokeh_port}", f"localhost:{self.port}", f"127.0.0.1:{self.port}"],
-            num_procs=1
+        for doc_server in self.doc_manager.doc_server.values():
+            doc_script = server_document(url=f'http://{self.doc_manager.bokeh_host}:{self.doc_manager.bokeh_port}{doc_server.url}')
+            embedded_docs.append(doc_script)
+
+        html = render_template(
+            'index.html',
+            script=embedded_docs[0],
+            template="Flask"
         )
 
-        LOGGER.info(f'bokeh service started at {self.bokeh_address}:{self.bokeh_port}!\n{applications}')
+        return html
 
-        server.start()
+    def serve(self, blocking: bool = True):
+        LOGGER.info(f'starting {self} service...')
 
-        # for url in applications:
-        #     server.io_loop.add_callback(server.show, url)
+        self.doc_manager.start()
+        self.flask.route(rule='/', methods=["GET"])(self.index)
 
-        server.io_loop.start()
+        if blocking:
+            return serve(app=self.flask, host=self.host, port=self.port)
 
-    def start(self):
-        self.bokeh_thread.start()
+        t = Thread(target=serve, kwargs=dict(app=self.flask, host=self.host, port=self.port))
+        t.start()
 
+        # a monkey patch to resolve flask double logging issues
+        for hdl in (logger := self.flask.logger).handlers:
+            logger.removeHandler(hdl)
 
-app = Flask('Backtest.WebApp')
-DOC_MANAGER = DocManager()
+        for hdl in (logger := LOGGER.root).handlers:
+            logger.removeHandler(hdl)
 
-
-def index():
-    from bokeh.embed import server_document
-
-    embedded_docs = []
-
-    for doc_server in DOC_MANAGER.doc_server.values():
-        doc_script = server_document(f'http://localhost:{DOC_MANAGER.bokeh_port}{doc_server.url}')
-        embedded_docs.append(doc_script)
-
-    return render_template(
-        'index.html',
-        script=embedded_docs[0],
-        template="Flask"
-    )
+    @property
+    def url(self) -> str:
+        if self.host == '0.0.0.0':
+            return f'http://localhost:{self.port}/'
+        else:
+            return f'http://{self.host}:{self.port}/'
 
 
-def start_app():
-    DOC_MANAGER.start()
-
-    app.route('/')(index)
-    app.run(port=DOC_MANAGER.port)
+def start_app(blocking: bool = True):
+    web_app = WebApp()
+    web_app.serve(blocking=blocking)
 
 
 if __name__ == '__main__':
