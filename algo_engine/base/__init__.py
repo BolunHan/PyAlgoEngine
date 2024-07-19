c6cf05334d0269bf5dfcad7c549acf1307f42d53
Bolun <Bolun.Han@outlook.com>
2024-07-19 17:26:08 +0800

    v0.5.4.post6

--- algo_engine/base/__init__.py
+++ algo_engine/base/__init__.py
@@ -22,7 +22,7 @@ from .console_utils import Progress, GetInput, GetArgs, count_ordinal, TerminalS
 
 __all__ = ['PROFILE',
            'FinancialDecimal',
-           'TransactionSide', 'MarketData', 'OrderBook', 'BarData', 'TickData', 'TransactionData', 'TradeData', 'OrderBookBuffer', 'BarDataBuffer', 'TickDataBuffer', 'TransactionDataBuffer',
+           'TransactionSide', 'MarketData', 'OrderBook', 'BarData', 'DailyBar', 'CandleStick', 'TickData', 'TransactionData', 'TradeData', 'OrderBookBuffer', 'BarDataBuffer', 'TickDataBuffer', 'TransactionDataBuffer',
            'TechnicalAnalysis',
            'OrderState', 'OrderType', 'TradeInstruction', 'TradeReport',
            'Progress', 'GetInput', 'GetArgs', 'count_ordinal', 'TerminalStyle', 'InteractiveShell', 'ShellTransfer']
