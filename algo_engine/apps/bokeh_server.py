25ec6fc2e725b3b889ad7a2cb7c21946fc2d21df
Han Bolun <Bolun.Han@outlook.com>
2024-11-27 17:31:37 +0800

    added more binding ports

--- algo_engine/apps/bokeh_server.py
+++ algo_engine/apps/bokeh_server.py
@@ -183,21 +183,25 @@ class DocManager(object):
                     websocket_origin.append(f"{addr.address}:{self.port}")
                     websocket_origin.append(f"{addr.address}:{self.bokeh_port}")
 
-        if f'localhost:{self.port}' not in websocket_origin:
-            websocket_origin.append(f'localhost:{self.port}')
-        if f'127.0.0.1:{self.port}' not in websocket_origin:
-            websocket_origin.append(f'127.0.0.1:{self.port}')
-        if f'localhost:{self.bokeh_port}' not in websocket_origin:
-            websocket_origin.append(f'localhost:{self.bokeh_port}')
-        if f'127.0.0.1:{self.bokeh_port}' not in websocket_origin:
-            websocket_origin.append(f'127.0.0.1:{self.bokeh_port}')
+        if _ := f'localhost:{self.port}' not in websocket_origin:
+            websocket_origin.append(_)
+        if _ := f'127.0.0.1:{self.port}' not in websocket_origin:
+            websocket_origin.append(_)
+        if _ := f'localhost:{self.bokeh_port}' not in websocket_origin:
+            websocket_origin.append(_)
+        if _ := f'127.0.0.1:{self.bokeh_port}' not in websocket_origin:
+            websocket_origin.append(_)
+        if _ := f'{self.bokeh_host}:{self.port}' not in websocket_origin:
+            websocket_origin.append(_)
+        if _ := f'{self.bokeh_host}:{self.bokeh_port}' not in websocket_origin:
+            websocket_origin.append(_)
 
         self.bokeh_server = Server(
             applications=self.doc_server,
             address='0.0.0.0',
             port=self.bokeh_port,
             check_unused_sessions_milliseconds=(self.bokeh_check_unused_sessions * 1000),
-            allow_websocket_origin=[f"{self.bokeh_host}:{self.bokeh_port}"] + websocket_origin,
+            allow_websocket_origin=websocket_origin,
             use_xheaders=True,
             # num_procs=1
         )
