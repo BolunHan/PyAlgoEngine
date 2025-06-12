ba353ebb8da0b39d8be8e2226bbe05919fde42df
Han Bolun <Bolun.Han@outlook.com>
2025-06-12 22:25:26 +0800

    added some utils functions to help cimport the objects

--- algo_engine/base/c_market_data/__init__.py
+++ algo_engine/base/c_market_data/__init__.py
@@ -5,7 +5,14 @@ from .c_candlestick import BarData, DailyBar
 from .c_market_data_buffer import MarketDataBuffer, MarketDataRingBuffer, MarketDataConcurrentBuffer
 from .c_trade_utils import OrderState, TradeInstruction, TradeReport
 
+
+def get_include():
+    import os
+    return os.path.dirname(__file__)
+
+
 __all__ = [
+    'get_include',
     "MarketData", "DataType", "OrderType", "InternalData", "FilterMode",
     "TransactionDirection", "TransactionOffset", "TransactionSide",
     "TransactionData", "TradeData", "OrderData",
