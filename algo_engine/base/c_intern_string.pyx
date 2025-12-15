22263b53b2ba43b2384f5dc457c41bd34dfde051
Han Bolun <Bolun.Han@outlook.com>
2025-12-15 15:55:23 +0800

    added ptr info to global for easier debugging and re-attaching

--- algo_engine/base/c_intern_string.pyx
+++ algo_engine/base/c_intern_string.pyx
@@ -89,10 +89,12 @@ cdef class InternStringPool:
 
 
 cdef istr_map* C_POOL = c_istr_map_new(0, C_ALLOCATOR)
+globals()['C_POOL'] = <uintptr_t> C_POOL
 cdef InternStringPool POOL = InternStringPool.c_from_header(C_POOL, True)
 globals()['POOL'] = POOL
 
 cdef istr_map* C_INTRA_POOL = c_istr_map_new(0, NULL)
+globals()['C_INTRA_POOL'] = <uintptr_t> C_INTRA_POOL
 cdef InternStringPool INTRA_POOL = InternStringPool.c_from_header(C_INTRA_POOL, True)
 globals()['INTRA_POOL'] = INTRA_POOL
 
