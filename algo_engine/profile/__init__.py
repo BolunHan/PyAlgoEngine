4534ea9fbcf980e133d1be234e4e038692fdab52
Han Bolun <Bolun.Han@outlook.com>
2026-04-07 17:41:21 +0800

    deprecating old profile module

--- algo_engine/profile/__init__.py
+++ algo_engine/profile/__init__.py
@@ -1,5 +1,13 @@
+import warnings
+
 from .c_base import PROFILE, PROFILE_DEFAULT, ProfileDispatcher as Profile
 from .c_cn import PROFILE_CN
 
+warnings.warn(
+    "`algo_engine.profile` is deprecated and will be removed in a future release. "
+    "Use `algo_engine.exchange_profile` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
 
 __all__ = ['PROFILE', 'Profile', 'PROFILE_DEFAULT', 'PROFILE_CN']
