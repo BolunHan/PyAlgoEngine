7866254f83ce76da0c80b7a9ac2f05b5c134cd05
Han Bolun <Bolun.Han@outlook.com>
2026-09-08 13:56:01 +0800

    feat: export SimMatchEx from backtest package

--- algo_engine/backtest/__init__.py
+++ algo_engine/backtest/__init__.py
@@ -11,9 +11,11 @@ def set_logger(logger: logging.Logger):
 
     replay.LOGGER = LOGGER.getChild('Replay')
     sim_match.LOGGER = LOGGER.getChild('SimMatch')
+    c_simmatch_ex.LOGGER = LOGGER.getChild('SimMatch')
 
 
 from .replay import PyDataScope, MarketDateCallable, MarketDataLoader, MarketDataBulkLoader, Replay, SimpleReplay, ProgressReplay
 from .sim_match import SimMatch
+from .c_simmatch_ex import SimMatchEx
 
-__all__ = ['PyDataScope', 'MarketDateCallable', 'MarketDataLoader', 'MarketDataBulkLoader', 'Replay', 'SimpleReplay', 'ProgressReplay', 'SimMatch']
+__all__ = ['PyDataScope', 'MarketDateCallable', 'MarketDataLoader', 'MarketDataBulkLoader', 'Replay', 'SimpleReplay', 'ProgressReplay', 'SimMatch', 'SimMatchEx']
